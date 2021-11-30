4e9e733cdb857b4f5a694671107f32662eb2982c
Łukasz Jabłoński <lukasz.j.pl@interia.pl>
2021-11-30 21:51:13 +0100

    Initial project structure customer

---
+++ customer/src/main/java/com/lukasz/customer/Customer.java
@@ -0,0 +1,14 @@
+package com.lukasz.customer;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Customer {
+
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+}
